e6fd406797ffef63fdbef5a0cecdf67104f518be
Phan Nhut Duy <duyphan2058@gmail.com>
2025-06-27 18:41:32 +0700

    Update HomeController.java

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,8 +1,11 @@
 package controller;
 
-import org.springframework.stereotype.Controller;
-
-@Controller
 public class HomeController {
-	
+	public static void main(String[] args) {
+		int a = 7;
+		int b = 10;
+		int c = a+b;
+		
+		System.out.println("SUM = "+c);
+	}
 }
