6d1b8d6945018ece4daecc10ad773b13c47b66b4
Phan Nhut Duy <duyphan2058@gmail.com>
2025-06-27 18:41:52 +0700

    Update HomeDAO.java

--- src/main/java/dao/HomeDAO.java
+++ src/main/java/dao/HomeDAO.java
@@ -1,9 +1,11 @@
 package dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import entity.Home;
 
 public interface HomeDAO extends JpaRepository<Home, String>{
-
+	Optional<Home> findById(String id);
 }
